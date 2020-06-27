1b6dc40b572b6fa0d478d3deda6514b35e542359
jayasting98 <jasting98@yahoo.com>
2020-06-28 00:57:02 +0800

    updated settings - added "enable notifications" setting that currently does nothing, added a confirmation dialog box for the delete account setting

---
+++ app/src/main/java/com/example/chemicalx/settings/DeleteAccountSettingDialogFragment.java
@@ -0,0 +1,61 @@
+package com.example.chemicalx.settings;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.chemicalx.R;
+
+public class DeleteAccountSettingDialogFragment extends DialogFragment {
+    private DeleteAccountSettingDialogListener listener;
+
+    /* The activity that creates an instance of this dialog fragment must
+     * implement this interface in order to receive event callbacks.
+     * Each method passes the DialogFragment in case the host needs to query it. */
+    public interface DeleteAccountSettingDialogListener {
+        public void onDeleteAccountSettingDialogDeleteClick(DialogFragment dialog);
+        public void onDeleteAccountSettingDialogCancelClick(DialogFragment dialog);
+    }
+
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            listener = (DeleteAccountSettingDialogListener) context;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(context.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.delete_account_setting_dialog_title)
+                .setMessage(R.string.delete_account_setting_dialog_message)
+                .setPositiveButton(R.string.delete_account_setting_dialog_affirmative,
+                        new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        listener.onDeleteAccountSettingDialogDeleteClick(
+                                DeleteAccountSettingDialogFragment.this);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        listener.onDeleteAccountSettingDialogCancelClick(
+                                DeleteAccountSettingDialogFragment.this);
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
